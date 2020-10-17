8557b735282756cc6ab0058696434f6e11c27495
Rakeshbaghel021 <rkrkumar021@gmail.com>
2020-10-17 21:47:04 +0530

    initial  commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// All Requires
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+// Mounting The Express Application
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Adding The Webpack To Run Application In The Single Enviroment!
+if (process.env.NODE_ENV === "development") {
+  var webpack = require("webpack");
+  var webpackConfig = require("./webpack.config");
+  var compiler = webpack(webpackConfig);
+
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    })
+  );
+
+  app.use(require("webpack-hot-middleware")(compiler));
+}
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+// Providing The Api Paths
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+// Exporting The Server File
+module.exports = app;
---
+++ client/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+function App() {
+  return <h1>helloReact</h1>;
+}
+export default App;
